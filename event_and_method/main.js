4823886889f5675c6d8ee3defb800322f3fc460d
Muhammad Asri Nasution <nasution.nam@gmail.com>
2019-11-16 17:18:11 +0700

    added mouseover method

--- event_and_method/main.js
+++ event_and_method/main.js
@@ -10,6 +10,9 @@ const app = new Vue({
             console.log(this.counter);
             if (this.counter == 10)
                 alert("Counter is at 10");
+        },
+        overTheBox() {
+            console.log("Over the green box")
         }
     }
 })
