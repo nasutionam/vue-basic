a829607177c608d0cc57209619f3bc5aa1dbc962
Muhammad Asri Nasution <nasution.nam@gmail.com>
2019-11-16 16:55:17 +0700

    new app initiation

---
+++ first_vue/main.js
@@ -0,0 +1,9 @@
+var app =  new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello World',
+        value: 100,
+        imgSrc: 'https://m.ayobandung.com/images-bandung/post/articles/2019/10/14/66788/cat-baby-4208534_960_720.jpg',
+        link: 'https://google.com'
+    }
+})
