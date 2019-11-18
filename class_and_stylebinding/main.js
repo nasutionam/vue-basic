c51f60015d33d2fd5c67aa3489201ba67e9a0071
Muhammad Asri Nasution <nasution.nam@gmail.com>
2019-11-18 09:56:45 +0700

    added method to change binding class

--- class_and_stylebinding/main.js
+++ class_and_stylebinding/main.js
@@ -1,7 +1,13 @@
 const app = new Vue({
     el: '#app',
     data: {
+        flag: true,
         styleObject: { backgroundColor: 'green', border: '5px solid orange'  }
+    },
+    methods: {
+        changeShape() {
+            this.flag = !this.flag
+        }
     }
    
 })
