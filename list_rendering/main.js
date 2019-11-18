47864ab28cc1f3797fb2b8b3545f343daf5ef2c7
Muhammad Asri Nasution <nasution.nam@gmail.com>
2019-11-18 10:12:13 +0700

    list rendering object user

--- list_rendering/main.js
+++ list_rendering/main.js
@@ -2,6 +2,26 @@ const app = new Vue({
     el: '#app',
   
    data: {
-       users: ["lorem", "ipsum", "dolor", "sit", "amet"]
+       users: ["lorem", "ipsum", "dolor", "sit", "amet"],
+
+       users2: [{
+           id: 567,
+           name: "lorem",
+           profession: "developer"
+       }, {
+           id: 568,
+           name: "ipsum",
+           profession: "devops"
+       },
+       {
+           id: 569,
+           name: "dolor",
+           profession: "admin"
+       },
+       {
+           id: 570,
+           name: "sit",
+           profession: "python developer"
+       }]
    }
 })
